ab7d75750bce6fb9196a2e04130b493c900834e5
github username <github email address>
2019-05-29 22:12:15 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-
+require 'pry'
 class Song
 
 
@@ -30,7 +30,7 @@ class Song
       self.send("#{property}=", value)
     end
   end
-
+  
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
@@ -40,7 +40,7 @@ class Song
   def table_name_for_insert
     self.class.table_name
   end
-
+  
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
@@ -57,7 +57,6 @@ class Song
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
-
 end
 
 
